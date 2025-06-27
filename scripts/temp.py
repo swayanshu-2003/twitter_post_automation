2a1db2491a08c60f092f7fea7a3a948894afca38
Swayanshu Sekhar <swayanshupanda650@gmail.com>
2025-06-27 16:43:52 +0530

    fixed workflow

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,11 +1,11 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-# from app.post_engine.funny_post_generator import generate_funnly_post_content
-from app.post_engine.publish_funny_post import fetch_post_and_publish
+from app.post_engine.funny_post_generator import generate_funnly_post_content
+# from app.post_engine.publish_funny_post import fetch_post_and_publish
 if __name__ == "__main__":
-    # print("Generating post content...")
-    # generate_funnly_post_content()
+    print("Generating post content...")
+    generate_funnly_post_content()
     
-    print("Posting to X...")
-    fetch_post_and_publish()
+    # print("Posting to X...")
+    # fetch_post_and_publish()
